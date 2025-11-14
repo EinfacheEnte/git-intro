45154d8edf4e1cb5ba42396ae6f913ee8bec47ba
oleschultz <einfacheente@gmail.com>
2025-11-14 15:18:15 +0100

    Added speed test code in each languages

---
+++ Sum.java
@@ -0,0 +1,12 @@
+public class Sum 
+{
+	public static void main(String[] args)
+	{
+
+	long sum = 0;
+	for(int i = 0;i<1000000000000f; i++)	
+		{
+		sum++;		
+		}
+	}
+}
---
+++ sum.py
@@ -0,0 +1,6 @@
+sum.py
+summe = 0
+for i in range(100000000):
+summe += i
+print(summe)
+
